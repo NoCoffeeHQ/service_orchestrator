d89aa96e2ef6e272cd177c5e44df4237d718763a
Didier Lafforgue <didier.lafforgue@gmail.com>
2022-12-30 10:25:35 +0100

    initial commit

---
+++ lib/service_orchestrator/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ServiceOrchestrator
+  VERSION = '0.1.0'
+end
---
+++ spec/service_orchestrator_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ServiceOrchestrator do
+  it 'has a version number' do
+    expect(ServiceOrchestrator::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
